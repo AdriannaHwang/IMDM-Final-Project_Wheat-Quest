1dd4d25d8838f25b5bd9d80a2d3339f16853b9b1
KayySims7896 <161513671+KayySims7896@users.noreply.github.com>
2024-05-09 15:21:09 -0400

    location initialized

---
+++ Wheat Quest/Assets/Initialize.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialize : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    void Start()
+    {
+        transform.position = new Vector3(-38,11.3f, -36.7f); 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
