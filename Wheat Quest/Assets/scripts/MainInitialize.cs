355187bca41a561690811e9e92d4c1859f6e562c
hchen114 <161513508+hchen114@users.noreply.github.com>
2024-05-09 19:54:11 -0400

    initialize script for mainscene, but not attached

---
+++ Wheat Quest/Assets/scripts/MainInitialize.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainInitialize : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    void Start()
+    {
+        transform.position = new Vector3(2.34f,-0.47f, -1.19f); 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
